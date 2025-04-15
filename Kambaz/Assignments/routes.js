d9cee1714b7d78e9da6c27831f04971644273c34
Tina <y634huan@uwaterloo.ca>
2025-04-14 20:13:57 -0400

    Fixed minor bug

--- Kambaz/Assignments/routes.js
+++ Kambaz/Assignments/routes.js
@@ -2,7 +2,7 @@ import * as assignmentsDao from "./dao.js";
 export default function AssignmentRoutes(app) {
   app.delete("/api/assignments/:aId", async (req, res) => {
     const { aId } = req.params;
-    const status = assignmentsDao.deleteAssignment(aId);
+    const status = await assignmentsDao.deleteAssignment(aId);
     res.send(status);
   });
   app.put("/api/assignments/:aId", async (req, res) => {
